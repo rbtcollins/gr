d318ee89dc5120aa9a0dbd29e767c9c61cfb5fcb
Robert Collins <robertc@robertcollins.net>
2023-03-03 20:52:51 +0100

    Add config subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(next_line_help = false)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Configure gr
+    Config {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Config {} => {
+            println!("config");
+        }
+    }
 }
